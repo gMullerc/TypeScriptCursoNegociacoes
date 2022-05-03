be12351b64cbc31726f15fb547921a41279cf6a2
gmullerc <guilherme.mcosta15@gmail.com>
2022-05-03 09:27:06 -0300

    segundo curso ts

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,12 @@
 import { NegociacaoController } from "./controllers/negociacao-Controller.js";
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error('nao foi possivel inciar a aplicação, verificar se o form existe');
+}
